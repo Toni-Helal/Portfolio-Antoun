ceb9e683bae4ed5d4661a3e028ba1c853c73cbf4
Antoun Helal <pixel.forge.palace@gmail.com>
2026-06-26 16:40:53 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,9 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  resolve: {
+    base: "/Portfolio-Antoun/",
+    plugins: [react(), tailwindcss(), viteSingleFile()],
+    resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
